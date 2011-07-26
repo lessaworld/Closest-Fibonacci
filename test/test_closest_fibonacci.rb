1aa66b5e5e20e80b36df6ff231f4e69bd330eae5
Andre Lessa <me@andrelessa.com>
2011-07-26 16:54:30 -0400

    Added Unit Tests

--- test/test_closest_fibonacci.rb
+++ test/test_closest_fibonacci.rb
@@ -1,7 +1,11 @@
 require 'helper'
+require 'closest_fibonacci'
 
 class TestClosestFibonacci < Test::Unit::TestCase
   should "match these pre-identified values" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+    assert(0, 0.closest_fibonacci)
+    assert(1, 0.closest_fibonacci)
+    assert(144, 156.closest_fibonacci)
+    assert(89, 99.closest_fibonacci)
   end
 end
